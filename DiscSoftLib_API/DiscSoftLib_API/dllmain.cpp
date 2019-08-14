eb3453f923d528906f4984720014e883dbe4212e
sst311212 <sst311212@gmail.com>
2019-08-14 18:41:16 +0800

    Add files via upload
    v5.1.0.1279
    Hook CreateFileW

--- DiscSoftLib_API/DiscSoftLib_API/dllmain.cpp
+++ DiscSoftLib_API/DiscSoftLib_API/dllmain.cpp
@@ -13,6 +13,9 @@ typedef BOOL(*detour_CryptQueryObject)(DWORD, const void*, DWORD, DWORD,
 	DWORD, DWORD*, DWORD*, DWORD*, HCERTSTORE*, HCRYPTMSG*, const void**);
 detour_CryptQueryObject fpCryptQueryObject = CryptQueryObject;
 
+typedef HANDLE(*detour_CreateFileW)(LPCWSTR, DWORD, DWORD, LPSECURITY_ATTRIBUTES, DWORD, DWORD, HANDLE);
+detour_CreateFileW fpCreateFileW = CreateFileW;
+
 BOOL Hooked_CryptQueryObject(
 	DWORD      dwObjectType,
 	const void *pvObject,
@@ -32,6 +35,21 @@ BOOL Hooked_CryptQueryObject(
 		dwFlags, pdwMsgAndCertEncodingType, pdwContentType, pdwFormatType, phCertStore, phMsg, ppvContext);
 }
 
+HANDLE Hooked_CreateFileW(
+	LPCWSTR               lpFileName,
+	DWORD                 dwDesiredAccess,
+	DWORD                 dwShareMode,
+	LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+	DWORD                 dwCreationDisposition,
+	DWORD                 dwFlagsAndAttributes,
+	HANDLE                hTemplateFile) {
+	auto lpLibName = StrStrIW(lpFileName, L"DiscSoftLib.dll");
+	if (lpLibName != NULL)
+		StrCpyW(lpLibName, DiscSoftLib);
+	return fpCreateFileW(lpFileName, dwDesiredAccess, dwShareMode,
+		lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
+}
+
 BOOL APIENTRY DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
 	if (dwReason == DLL_PROCESS_ATTACH)
@@ -39,6 +57,7 @@ BOOL APIENTRY DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 		DiscSoftLib_Init();
 		DetourTransactionBegin();
 		DetourAttach(&(LPVOID&)fpCryptQueryObject, Hooked_CryptQueryObject);
+		DetourAttach(&(LPVOID&)fpCreateFileW, Hooked_CreateFileW);
 		DetourTransactionCommit();
 	}
 	return TRUE;
